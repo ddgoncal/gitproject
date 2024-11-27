e506d3e5dc180b1bbeb611052230c504a0cea5b5
Diogo Goncalves <diogo.goncalves@icapitalnetwork.com>
2024-11-27 18:55:52 +0000

    novo commit

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -31,7 +31,7 @@ export default function TabTwoScreen() {
         </ThemedText>
         <ThemedText>
           The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator. mais texto
+          sets up the tab navigator. mssais texto
         </ThemedText>
         <ExternalLink href="https://docs.expo.dev/router/introduction">
           <ThemedText type="link">Learn more</ThemedText>
